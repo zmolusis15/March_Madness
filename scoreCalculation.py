bc62dd4e98000cd363e643bb1b410f8159a4cce7
zmolusis15 <molusis@wisc.edu>
2025-03-03 19:45:59 -0600

    Add files via upload

--- scoreCalculation.py
+++ scoreCalculation.py
@@ -61,7 +61,7 @@ def rating_boosts (row, opponent):
     boost_rating = 0
 
     #Momentum Boost
-    momentum_boost_set = {"Kansas","Auburn","North Carolina"}
+    momentum_boost_set = {"North Carolina"}
     if row['team'] in momentum_boost_set:
         boost_rating += 0.1
         
@@ -80,7 +80,7 @@ def simulate_game (team_name1, team_name2, df):
     team1_score += rating_boosts(team1_row,team2_row)
     team2_score += rating_boosts(team1_row,team2_row)
 
-    rating_diff = (team1_score - team2_score) * 6  # Scale factor to adjust win probability distribution
+    rating_diff = (team1_score - team2_score) * 7  # Scale factor to adjust win probability distribution
     win_prob_team1 = 1 / (1 + np.exp(-rating_diff))  
 
     # Simulate game outcome
